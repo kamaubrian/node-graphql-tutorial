5edcb2acd61670d09c41be2a2546c045460fddf2
mtotodev05 <mtotodev05@gmail.com>
2019-02-20 21:47:42 +0300

    Setup firebase

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const http  = require('http');
+const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
-let createServer = async()=>{
-    try{
+let createServer = async () => {
+    try {
         await server.listen(port);
-        console.log('Server Running on Port',port)
-    }catch (e) {
-        console.log('Error Creating Node Server',e.message);
+        console.log('Server Running on Port', port)
+    } catch (e) {
+        console.log('Error Creating Node Server', e.message);
     }
 };
 
-createServer();
+createServer()
+    .then()
+    .catch(err=>{
+        console.error(err.message);
+    });
 
 module.exports = server;
