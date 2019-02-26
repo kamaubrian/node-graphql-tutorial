834819e8a6e2877c555177cc938bf5d4c52e3a89
mtotodev05 <mtotodev05@gmail.com>
2019-02-26 08:22:04 +0300

    Pass fetch tests

--- api/test/index.js
+++ api/test/index.js
@@ -2,14 +2,45 @@ const request = require('supertest');
 const expect = require('expect');
 const server = require('../../server');
 
-afterEach(()=>{
+afterEach(() => {
     server.close();
-})
+});
 
-describe('/Testing Correct Urls',()=>{
-    it('it should return 404 on invalid route',(done)=>{
+describe('/Testing Correct Urls', () => {
+    it('it should return 404 on invalid route', (done) => {
         request(server)
             .get('/api/v1/invalid')
-            .expect(404,done);
+            .expect(404, done);
     })
+});
+
+describe('/Testing Get Capabilities', () => {
+    let requestBody = {
+        // language=GraphQL Schema
+        query: `
+           query{
+                posts{
+                    description
+                    author
+                }
+           }   
+        `
+    };
+    it('it should fetch all posts', (done) => {
+        request(server)
+            .post('/api/v1/graphql')
+            .send(requestBody)
+            .expect(200, done);
+    });
+});
+
+describe('/Testing Post Capabilities', () => {
+    let requestBody = {
+        // language=GraphQL Schema
+        query:`
+        
+        
+        `
+    };
+
 })
